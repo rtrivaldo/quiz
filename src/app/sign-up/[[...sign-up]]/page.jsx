4a03b889234a568929d9e9f9ed3a4fa6bb0528b1
rtrivaldo <rtrivaldo05@gmail.com>
2024-10-12 08:07:33 +0700

    fix: missing return in signup page

--- src/app/sign-up/[[...sign-up]]/page.jsx
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -1,7 +1,9 @@
 import { SignUp } from '@clerk/nextjs';
 
 export default function SignUpPage() {
-  <div className='flex justify-center items-center h-screen'>
-    <SignUp />
-  </div>;
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <SignUp />
+    </div>
+  );
 }
